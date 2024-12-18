745300062ba487b0edd1248683d8fb7a4462b518
Khaled ElGabry <iKhaledElGabry@gmail.com>
2024-12-18 22:48:51 +0200

    adding a new solution for Valid Palindrome problem

--- java/125-valid-palindrome.java
+++ java/125-valid-palindrome.java
@@ -25,4 +25,36 @@ class Solution{
 
         return true;
     }
+
+
+    public boolean isPalindromeASCIIOnly(String s) {
+        String regex = "\\A\\p{ASCII}*\\z";
+        if (!s.matches(regex)) {
+            throw new IllegalArgumentException("Invalid, ASCII Characters only");
+        }
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            // skip non alphanumeric characters
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            // compare characters
+            if (Character.toLowerCase(s.charAt(left)) !=
+                    Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
 }
