a966f8130d90e92460591828baf99c629688512a
Khaled ElGabry <iKhaledElGabry@gmail.com>
2025-01-21 21:45:39 +0200

    Modify solution for Plus One problem

--- java/66-plus-one.java
+++ java/66-plus-one.java
@@ -1,17 +1,23 @@
 class Solution {
     public int[] plusOne(int[] digits) {
 
-        // validations
+        // validation
         if (digits.length < 1 || digits.length > 100) {
-            throw new IllegalArgumentException("invalid");
+            throw new IllegalArgumentException("Invalid Array input");
         }
 
+        // traverse the array from the last digit to the first
         for (int i = digits.length - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
+
+            if (digits[i] < 0 || digits[i] > 9) { // validation for for each digit
+                throw new IllegalArgumentException("Invalid Array item: "+ digits[i]);
+            }
+
+            if (digits[i] < 9) {  // if current digit is less than 9, increment it by one and return
                 digits[i]++;
                 return digits;
             }
-            digits[i] = 0;
+            digits[i] = 0; // if digit is 9, set it to 0 (carry over to the next digit)
         }
 
         int[] result = new int[digits.length + 1];
