d0aba0b5d2eb3a1cf42b19fc86d3eb92b96e562b
Khaled ElGabry <iKhaledElGabry@gmail.com>
2025-01-30 19:59:10 +0200

    Add comments and make small modify on my solution for the Minimum Average of Smallest and Largest Elements, make it more readable

--- java/3194-minimum-average-of-smallest-and-largest-elements.java
+++ java/3194-minimum-average-of-smallest-and-largest-elements.java
@@ -3,24 +3,25 @@ public class Solution {
 
         // validation
         if (nums.length < 2 || nums.length > 50 || nums.length % 2 == 1) {
-            throw new IllegalArgumentException("Invalid");
+            throw new IllegalArgumentException("Invalid input");
         }
 
-        Arrays.sort(nums);
+        Arrays.sort(nums); // sort the input array
 
-        double minAvg = Double.MAX_VALUE;
+        double minAvg = Double.MAX_VALUE; // to track the minimum average found
         int left = 0;
         int right = nums.length - 1;
 
+        // iterates over half of the array
         for (int i = 0; i < nums.length / 2; i++) {
 
             if (nums[i] < 1 || nums[i] > 50) {
-                throw new IllegalArgumentException("Invalid");
+                throw new IllegalArgumentException("Invalid number value: "+ nums[i]);
             }
 
             double avg = (nums[left] + nums[right]) / 2.0;
 
-            if (avg < minAvg) {
+            if (avg < minAvg) { // updates minAvg if a new minimum is found
                 minAvg = avg;
             }
 
