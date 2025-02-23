08e87b6eec11a82a82c3302d210ca364ffa4e558
Khaled ElGabry <iKhaledElGabry@gmail.com>
2025-02-23 23:57:18 +0200

    Remove unnecessary validation

--- java/7-reverse-integer.java
+++ java/7-reverse-integer.java
@@ -1,32 +1,26 @@
-    class Solution {
-        public int reverse(int x) {
+class Solution {
+    public int reverse(int x) {
 
-            // validation
-            if (x == 0)
+        // validation
+        if (x == 0) return 0;
+
+        int result = 0;
+        while (x != 0) {
+            int digit = x % 10; // last digit
+
+            // check for overflow before multiplying
+            if (result > Integer.MAX_VALUE / 10 ||
+                    (result == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {
                 return 0;
-            if (x == Integer.MIN_VALUE)
-                return 0;
-
-            int result = 0;
-            while (x != 0) {
-
-                int digit = x % 10; // last digit
-
-                // check for overflow before multiplying
-                if (result > Integer.MAX_VALUE / 10 ||
-                        (result == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {
-                    return 0;
-                }
-                if (result < Integer.MIN_VALUE / 10 ||
-                        (result == Integer.MIN_VALUE / 10 && digit < Integer.MIN_VALUE % 10)) {
-                    return 0;
-                }
-
-                result = result * 10 + digit;
-
-                x = x / 10; // remove the last digit from x
             }
+            if (result < Integer.MIN_VALUE / 10 ||
+                    (result == Integer.MIN_VALUE / 10 && digit < Integer.MIN_VALUE % 10)) {
+                return 0;
+            }
+            result = result * 10 + digit;
 
-            return result;
+            x = x / 10; // remove the last digit from x
         }
+        return result;
     }
+}
