ce32e13dc83e0b67eb1b2678735369d372b09a4d
Khaled ElGabry <iKhaledElGabry@gmail.com>
2025-02-26 14:29:36 +0200

    Remove unnecessary validation

--- java/26-remove-duplicates-from-sorted-array.java
+++ java/26-remove-duplicates-from-sorted-array.java
@@ -1,11 +1,6 @@
 class Solution{
     public int removeDuplicates(int[] nums) {
-
-        // validation
-        if(nums.length < 1 || nums.length > 30_000){
-            throw new IllegalArgumentException("Invalid input");
-        }
-
+        
         int writeIndex = 1; // position to write the next unique item
 
         for (int i = 1; i < nums.length; i++) {
