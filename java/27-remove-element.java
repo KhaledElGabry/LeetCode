57a46921f132b7cae96b9fa8891f0167e66a1512
Khaled ElGabry <iKhaledElGabry@gmail.com>
2025-02-27 23:59:43 +0200

    Remove unnecessary validation

--- java/27-remove-element.java
+++ java/27-remove-element.java
@@ -1,14 +1,6 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-
-        // validation
-        if (nums.length < 0 || nums.length > 100) {
-            throw new IllegalArgumentException("Invalid input array");
-        }
-        if (val < 0 || val > 100) {
-            throw new IllegalArgumentException("Invalid input val");
-        }
-
+        
         int k = 0; // count of non-val elements
 
         for (int i = 0; i < nums.length; i++) {
