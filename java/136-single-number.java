ac3355f8ff2ae088149bddfff20e86966772aa50
Khaled ElGabry <57463818+KhaledElGabry@users.noreply.github.com>
2025-03-04 00:00:14 +0200

    Remove unnecessary validation

--- java/136-single-number.java
+++ java/136-single-number.java
@@ -1,11 +1,6 @@
 class Solution {
     public int singleNumber(int[] nums) {
 
-        // validation
-        if (nums.length < 1 || nums.length > 30000) {
-            throw new IllegalArgumentException("Invalid Input");
-        }
-
         int result = 0;
         for (int number : nums) {
             result = number ^ result;
