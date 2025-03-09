9234553a951dfabb6970c465388d0cacac14f2fc
Khaled ElGabry <iKhaledElGabry@gmail.com>
2025-03-09 21:22:36 +0200

    remove unnecessary validation

--- algorithm-analysis/3194-minimum-average-of-smallest-and-largest-elements-Analysis.md
+++ algorithm-analysis/3194-minimum-average-of-smallest-and-largest-elements-Analysis.md
@@ -7,19 +7,15 @@
 public class Solution {
     public double minimumAverage(int[] nums) {
 
-        Arrays.sort(nums);
+        Arrays.sort(nums); // sort the input array
 
         double minAvg = Double.MAX_VALUE;
         int left = 0;
         int right = nums.length - 1;
 
         for (int i = 0; i < nums.length / 2; i++) {
-
-            if (nums[i] < 1 || nums[i] > 50) {
-                throw new IllegalArgumentException("Invalid");
-            }
-
             double avg = (nums[left] + nums[right]) / 2.0;
+            
             if (avg < minAvg) {
                 minAvg = avg;
             }
@@ -31,7 +27,6 @@ public class Solution {
         return minAvg;
     }
 }
-
 ```
 
 [//]: # (#### Go:)
--- java/3194-minimum-average-of-smallest-and-largest-elements.java
+++ java/3194-minimum-average-of-smallest-and-largest-elements.java
@@ -3,19 +3,14 @@ public class Solution {
 
         Arrays.sort(nums); // sort the input array
 
-        double minAvg = Double.MAX_VALUE; // to track the minimum average found
+        double minAvg = Double.MAX_VALUE;
         int left = 0;
         int right = nums.length - 1;
 
-        // iterates over half of the array
         for (int i = 0; i < nums.length / 2; i++) {
-
-            if (nums[i] < 1 || nums[i] > 50) {
-                throw new IllegalArgumentException("Invalid number value: "+ nums[i]);
-            }
-
             double avg = (nums[left] + nums[right]) / 2.0;
-            if (avg < minAvg) { // updates minAvg if a new minimum is found
+
+            if (avg < minAvg) {
                 minAvg = avg;
             }
 
